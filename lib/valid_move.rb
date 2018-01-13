e2116931964461cbba1b115c2fad28f66083c232
Johannah Sims <jjrsims@gmail.com>
2018-01-13 19:47:25 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,4 @@
 def valid_move?(board,position)
-  position.to_i.between?(1,9) && !position_taken?(board, position.to_i - 1)
-end
-
-def position_taken?(board,position)
   return false if [" ", "", nil].include?(board[position])
   return true if ["X", "O"].include?(board[position])
   raise "#{board[position]} is not a valid move"
